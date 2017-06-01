30384b191971ce47fedcf9e409761fd9e5a95ab6
Carlos Chapi <carloswaldo@babelruins.org>
2017-06-01 13:30:23 -0500

    Various improvements for the translation memory plugin.

--- plugins/base_tm.py
+++ plugins/base_tm.py
@@ -3,22 +3,17 @@ from fuzzywuzzy import fuzz
 import sqlite3, os
 
 class plugin_thread(QtCore.QThread):
-	begining = QtCore.pyqtSignal(int)
-	progress = QtCore.pyqtSignal(int, object)
-	finished = QtCore.pyqtSignal(int)
+	finished = QtCore.pyqtSignal(object)
 	
-	def __init__(self, options, begining_callback, progress_callback, finished_callback, parent=None):
+	def __init__(self, options, finished_callback, parent=None):
 		QtCore.QThread.__init__(self, parent)
 		
 		self.options = options
-		self.begining.connect(begining_callback)
-		self.progress.connect(progress_callback)
 		self.finished.connect(finished_callback)
 		
 		self.aborted = False
 	
 	def run(self):
-		limit = 20
 		tm_db = sqlite3.connect(self.options['project_file_path'])
 		tm_cursor = tm_db.cursor()
 		matching_segments = {}
@@ -36,7 +31,7 @@ class plugin_thread(QtCore.QThread):
 		list_of_arguments = list(matching_segments.keys())
 		placeholder = '?'
 		placeholders = ', '.join(placeholder for x in list_of_arguments)
-		query = """	SELECT source_segments.segment_id, source_segments.segment, variants.segment
+		query = """	SELECT source_segments.segment_id, source_segments.segment, variants.segment, variants.source_file
 					FROM variants
 					JOIN source_segments ON variants.source_segment = source_segments.segment_id
 					WHERE source_segments.segment_id IN ({})
@@ -46,24 +41,16 @@ class plugin_thread(QtCore.QThread):
 		list_of_arguments.append(self.options['filename'])
 		list_of_arguments.append(self.options['segment_id'])
 		
-		if self.aborted:
-			return
-		
 		result = []
-		c = 0
 		for row in tm_cursor.execute(query, list_of_arguments):
 			result.append(row + (matching_segments[row[0]], ))
-			c = c + 1
-			if(c>=limit):
+			if self.aborted:
 				break
 
 		tm_db.close()
 		
 		if not self.aborted:
-			self.begining.emit(len(result))
-			for index, row in enumerate(result):
-				self.progress.emit(index, row)
-			self.finished.emit(len(result))
+			self.finished.emit(result)
 
 class main_widget(QtWidgets.QGroupBox):
 	def __init__(self):
@@ -75,12 +62,13 @@ class main_widget(QtWidgets.QGroupBox):
 		main_layout = QtWidgets.QGridLayout(self)
 		
 		self.candidates_box = QtWidgets.QTableWidget(self)
-		self.candidates_box.setColumnCount(3)
-		self.candidates_box.setHorizontalHeaderLabels(["%", "Source text", "Target text"])
+		self.candidates_box.setColumnCount(4)
+		self.candidates_box.setHorizontalHeaderLabels(["%", "Source text", "Target text", "File"])
 		self.table_header = self.candidates_box.horizontalHeader()
 		self.table_header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
 		self.table_header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
 		self.table_header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+		self.table_header.setSectionResizeMode(3, QtWidgets.QHeaderView.Interactive)
 		self.candidates_box.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
 		self.candidates_box.verticalHeader().hide()
 		self.candidates_box.sortItems(0, QtCore.Qt.DescendingOrder)
@@ -96,13 +84,11 @@ class main_widget(QtWidgets.QGroupBox):
 		status_layout = QtWidgets.QHBoxLayout()
 		self.status_label = QtWidgets.QLabel("Ready.")
 		status_layout.addWidget(self.status_label)
-		self.progress_bar = QtWidgets.QProgressBar(self)
-		status_layout.addWidget(self.progress_bar)
 		
 		main_layout.addWidget(self.candidates_box, 0, 0)
 		main_layout.addLayout(status_layout, 1, 0)
 		
-		self.progress_bar.setVisible(False)
+		self.on_finish_running = False
 	
 	def contextMenuEvent(self, pos):
 		self.target_text = self.parent().parent().parent().target_text
@@ -136,33 +122,44 @@ class main_widget(QtWidgets.QGroupBox):
 		self.target_text.setText(self.candidates_box.item(self.candidates_box.currentRow(), 1).text())
 		
 	def main_action(self, options):
-		self.candidates_box.setSortingEnabled(False)
-		
 		if hasattr(self, 'plugin_thread'):
 			self.plugin_thread.aborted = True
 			self.plugin_thread.quit()
-		self.plugin_thread = plugin_thread(options, self.onBegining, self.onProgress, self.onFinish, self)
+		self.plugin_thread = plugin_thread(options, self.onFinish, self)
 		self.status_label.setText("Searching for fuzzy matches...")
 		self.plugin_thread.start()
 	
-	def onBegining(self, count):
-		self.candidates_box.setRowCount(count)
-		
-	def onProgress(self, index, row):
-		percent_widget = QtWidgets.QTableWidgetItem()
-		percent_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[3]))
-		self.candidates_box.setItem(index, 0, percent_widget)
-		
-		source_widget = QtWidgets.QTableWidgetItem()
-		source_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[1]))
-		self.candidates_box.setItem(index, 1, source_widget)
-		
-		target_widget = QtWidgets.QTableWidgetItem()
-		target_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[2]))
-		self.candidates_box.setItem(index, 2, target_widget)
-		
-		QtWidgets.QApplication.processEvents()
-	
-	def onFinish(self, count):
+	def onFinish(self, result):
+		self.candidates_box.setSortingEnabled(False)
+		on_finish_running = True
+		total = str(len(result))
+		#self.candidates_box.setRowCount(0)
+		self.candidates_box.setRowCount(len(result))
+		for index, row in enumerate(result):
+			if index > self.candidates_box.rowCount():
+				print(index, self.candidates_box.rowCount())
+				return
+			percent_widget = QtWidgets.QTableWidgetItem()
+			percent_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[4]))
+			if row[4] == 100:
+				percent_widget.setBackground(QtGui.QColor(0, 255, 0))
+			else:
+				percent_widget.setBackground(QtGui.QColor(255, 255, 0))
+			self.candidates_box.setItem(index, 0, percent_widget)
+			
+			source_widget = QtWidgets.QTableWidgetItem()
+			source_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[1]))
+			self.candidates_box.setItem(index, 1, source_widget)
+			
+			target_widget = QtWidgets.QTableWidgetItem()
+			target_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[2]))
+			self.candidates_box.setItem(index, 2, target_widget)
+			
+			file_widget = QtWidgets.QTableWidgetItem()
+			file_widget.setData(QtCore.Qt.EditRole, QtCore.QVariant(row[3]))
+			self.candidates_box.setItem(index, 3, file_widget)
+			
+			self.status_label.setText('Loading match ' + str(index + 1) + ' of ' + total + '.')
+			QtWidgets.QApplication.processEvents()
 		self.candidates_box.setSortingEnabled(True)
-		self.status_label.setText(str(count) + ' matches found.')
+		self.status_label.setText('Ready.')
